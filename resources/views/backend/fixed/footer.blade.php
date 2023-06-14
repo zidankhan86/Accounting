3535db4d15d9a74602d3b741d86e327c13248065
zidankhan86 <zidankhan718@gmail.com>
2023-06-14 11:40:25 +0600

    footer

---
+++ resources/views/backend/fixed/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; 2023 <div class="bullet"></div> All Right Reserved <a href="https://nauval.in/">Dterms Ltd</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+  </footer>
