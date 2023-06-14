8dd95d5c4bec689f2fc76e3dd42248794f157b96
zidankhan86 <zidankhan718@gmail.com>
2023-06-14 16:04:19 +0600

    Validation

--- app/Http/Controllers/ManageAccountsController.php
+++ app/Http/Controllers/ManageAccountsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ManageAccount;
 use Illuminate\Http\Request;
+use App\Models\ManageAccount;
+use Illuminate\Support\Facades\Validator;
 
 class ManageAccountsController extends Controller
 {
@@ -13,21 +14,40 @@ class ManageAccountsController extends Controller
         return view('backend.pages.manageAccount.addAccount');
     }
 
-  public function addAccountCreate(Request $request){
+         public function addAccountCreate(Request $request){
 
-    //dd($request->all());
+            $validator = Validator::make($request->all(), [
+                'account_holder_name' => 'required|string',
+                'bank_name' => 'required|string',
+                'account_number' => 'required|string',
+                'opening_balance' => 'required|numeric',
+                'contact_number' => 'required|string',
+                'bank_address' => 'required|string',
+            ]);
 
-    ManageAccount::create([
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
 
-        "account_holder_name"=>$request->account_holder_name,
-        "bank_name"=>$request->bank_name,
-        "account_number"=>$request->account_number,
-        "opening_balance"=>$request->opening_balance,
-        "contact_number"=>$request->contact_number,
-        "bank_address"=>$request->bank_address,
+            // Validation passed, create account
+            ManageAccount::create($request->all());
 
-    ]);
-    return back();
+
+
+
+         //dd($request->all());
+
+         ManageAccount::create([
+
+            "account_holder_name"   =>$request->account_holder_name,
+            "bank_name"             =>$request->bank_name,
+            "account_number"        =>$request->account_number,
+            "opening_balance"       =>$request->opening_balance,
+            "contact_number"        =>$request->contact_number,
+            "bank_address"          =>$request->bank_address,
+
+         ]);
+         return back();
 
   }
 
