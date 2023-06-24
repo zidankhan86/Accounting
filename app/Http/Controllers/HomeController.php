48e67a28d9bc7c17bd2eeb34dd7d926a80d5830c
zidankhan86 <zidankhan718@gmail.com>
2023-06-24 17:38:55 +0600

    Home to Report

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ManageAccount;
 
 class HomeController extends Controller
 {
     public function home(){
-        return view('backend.pages.home');
+        // return view('backend.pages.home');
+        $accounts = ManageAccount::simplePaginate(8);
+        return view('backend.pages.report.report',compact('accounts'));
     }
 }
--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -9,7 +9,7 @@
       <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
         <li class="dropdown active">
-          <a href="{{route('home')}}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+          <a href="{{route('report')}}" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
         </li>
         <li class="menu-header">Starter</li>
         <li class="dropdown">
