31f5c65a28f93d699c3d2f66407f10987be0b61e
zidankhan86 <zidankhan718@gmail.com>
2023-08-28 18:07:21 +0600

    paginate

--- app/Models/AccountType.php
+++ app/Models/AccountType.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AccountType extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    /**
+     * Get all of the comments for the AccountType
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function AccountType(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'id', 'id');
+    }
 }
