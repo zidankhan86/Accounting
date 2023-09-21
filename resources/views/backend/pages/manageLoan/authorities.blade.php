d9923fd25c9052997c8d754d4aa2a703922f34b2
zidankhan86 <zidankhan718@gmail.com>
2023-09-21 16:22:09 +0600

    Ajax request

--- resources/views/backend/pages/manageLoan/authorities.blade.php
+++ resources/views/backend/pages/manageLoan/authorities.blade.php
@@ -1,110 +1,145 @@
 @extends('backend.master')
 @section('content')
 
-
 <style>
     .center {
-    text-align: center;
+        text-align: center;
     }
-
 </style>
 
-
-
 <div class="col-12 col-md-12 col-lg-12">
-
-
     <div class="card">
-      <div class="card-header">
-        <h4 class="text-center">+Add Authorities Form</h4>
-      </div>
-    <form action="{{route('add.authorities.create')}}" method="POST" enctype="multipart/form-data">
-        @csrf
-
-      <div class="card-body">
-        <div class="form-row">
-            <div class="form-group col-md-12">
-                <label for="inputName1">Authorities Name</label>
-                <input type="text" name="name" class="form-control" id="inputName1" placeholder="Authorities Name">
-
-                @error('account_holder_name')
-
-                <small class="text-danger">{{$message}}</small>
-
-                @enderror
-              </div>
-
-          <div class="form-group col-md-6">
-            <label for="inputName2">Email</label>
-           <input class="form-control" type="email" name="email" placeholder="Email">
-
-            @error('bank_name')
-            <p class="text-danger">{{$message}}</p>
-            @enderror
-           </div>
-
-          <div class="form-group col-md-6">
-            <label for="inputBalance1">Contact Number</label>
-            <input type="tel" name="number" class="form-control" id="inputBalance1" placeholder="Contact Number">
-            @error('account_number')
-            <small class="text-danger">{{$message}}</small>
-            @enderror
-           </div>
-
-
-           <div class="form-group col-md-6">
-            <label for="inputName2">Cash Credit (CC) Loan Limit</label>
-           <input class="form-control" type="number" name="cash_limit" value="100000" placeholder="100000">
-
-            @error('cash_credit')
-            <p class="text-danger">{{$message}}</p>
-            @enderror
-           </div>
-
-          <div class="form-group col-md-6">
-            <label for="inputAddress">Address </label>
-            <input type="text" name="address" class="form-control"  placeholder="Enter an address ">
-            @error('address')
-            <small class="text-danger">{{$message}}</small>
-            @enderror
-           </div>
-
-           <div class="form-group col-md-12">
-            <label for="inputAddress">Account Status</label>
-            <input type="hidden" name="status" class="form-control " id="inputAddress" placeholder="Account Status">
-              <select class="form-control" name="status" id="">
-                <option value="Active">Active</option>
-                <option value="Inactive">Inactive</option>
-              </select>
-
-            @error('status')
-            <small class="text-danger">{{$message}}</small>
-            @enderror
-          </div>
-
-          <div class="form-group col-md-12" style="width: 80%;">
-            <label for="inputAddress" style="font-size: 18px;">Note</label>
-            <textarea name="note" class="form-control" id="inputAddress" placeholder="Write your note here....." style="font-size: 16px; height: 150px; padding: 10px; resize: vertical;"></textarea>
-
-            @error('note')
-            <small class="text-danger" style="font-size: 14px;">{{$message}}</small>
-            @enderror
-          </div>
-
-
-
-
+        <div class="card-header">
+            <h4 class="text-center">+Add Authorities Form</h4>
         </div>
+        <form action="{{ route('add.authorities.create') }}" method="POST" enctype="multipart/form-data" id="authoritiesForm">
+            @csrf
 
-        <div class="center">
-            <button type="submit" class="btn btn-success ">Submit</button>
-        </div>
-      </div>
+            <div class="card-body">
+                <div class="form-row">
+                    <div class="form-group col-md-12">
+                        <label for="name">Authorities Name</label>
+                        <input type="text" name="name" class="form-control" id="name" placeholder="Authorities Name">
 
+                        @error('name')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="email">Email</label>
+                        <input class="form-control" type="email" name="email" id="email" placeholder="Email">
+
+                        @error('email')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="number">Contact Number</label>
+                        <input type="tel" name="number" class="form-control" id="number" placeholder="Contact Number">
+
+                        @error('number')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="cash_limit">Cash Credit (CC) Loan Limit</label>
+                        <input class="form-control" type="number" name="cash_limit" id="cash_limit" value="100000"
+                            placeholder="100000">
+
+                        @error('cash_limit')
+                        <p class="text-danger">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-6">
+                        <label for="address">Address</label>
+                        <input type="text" name="address" class="form-control" id="address"
+                            placeholder="Enter an address">
+
+                        @error('address')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-12">
+                        <label for="status">Account Status</label>
+                        <input type="hidden" name="status" class="form-control" id="status"
+                            placeholder="Account Status">
+                        <select class="form-control" name="status" id="status">
+                            <option value="Active">Active</option>
+                            <option value="Inactive">Inactive</option>
+                        </select>
+
+                        @error('status')
+                        <small class="text-danger">{{ $message }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group col-md-12" style="width: 80%;">
+                        <label for="note" style="font-size: 18px;">Note</label>
+                        <textarea name="note" class="form-control" id="note"
+                            placeholder="Write your note here....."
+                            style="font-size: 16px; height: 150px; padding: 10px; resize: vertical;"></textarea>
+
+                        @error('note')
+                        <small class="text-danger" style="font-size: 14px;">{{ $message }}</small>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="center">
+                    <button type="button" class="btn btn-success" id="submit">Submit</button>
+                </div>
+            </div>
         </form>
-
-      </div>
     </div>
-  </div>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const authoritiesForm = document.getElementById('authoritiesForm');
+        const submitButton = document.getElementById('submit');
+
+        submitButton.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the form from submitting directly
+
+            // Perform client-side validation
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+            const number = document.getElementById('number').value;
+            const cashLimit = document.getElementById('cash_limit').value;
+            const address = document.getElementById('address').value;
+
+            if (!name || !email || !number || !cashLimit || !address) {
+                toastr.error('All fields are required.');
+                return;
+            }
+
+            // Additional client-side validation logic can be added here
+
+            // If all validation passes, proceed with sending the AJAX request
+            const formData = new FormData(authoritiesForm);
+
+            axios.post("{{ route('add.authorities.create') }}", formData)
+                .then(function (response) {
+                    // Handle success, you can show a success message or redirect here if needed
+                    console.log(response.data);
+                    // Reset the form if the submission was successful
+                    authoritiesForm.reset();
+                    // You can add a success message or toast here
+                    toastr.success('Authorities added successfully.');
+                })
+                .catch(function (error) {
+                    // Handle errors, you can display an error message here if needed
+                    console.error(error);
+                    // You can add an error message or toast here
+                    toastr.error('An error occurred while adding the authorities');
+                });
+        });
+    });
+</script>
 
 @endsection
